92e69a183e27c69931f5948abec1d134b7568f39
Yulan Lin <yulan.lin.2014@gmail.com>
2021-12-03 20:59:25 -0800

    iterext

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,8 @@
 use anyhow::Context;
 use itertools::Itertools;
 
+use crate::library::IterExt;
+
 pub fn part1(input: &str) -> anyhow::Result<usize> {
     let numbers: Vec<i32> = input
         .split_whitespace()
@@ -12,19 +14,17 @@ pub fn part1(input: &str) -> anyhow::Result<usize> {
 }
 
 pub fn part2(input: &str) -> anyhow::Result<usize> {
-    let numbers: Vec<i32> = input
+    input
         .split_whitespace()
         .map(|token| token.parse())
-        .try_collect()
-        .context("failed to parse integer")?;
-
-    let chunk_sums: Vec<i32> = numbers
-        .windows(3)
-        .map(|chunk| chunk.iter().copied().sum())
-        .collect();
-
-    Ok(chunk_sums
-        .windows(2)
-        .filter(|pair| pair[0] < pair[1])
-        .count())
+        .use_oks(|numbers| {
+            numbers
+                .map(|a: u32| a)
+                .streaming_windows()
+                .map(|[a, b, c]| a + b + c)
+                .streaming_windows()
+                .filter(|[a, b]| a < b)
+                .count()
+        })
+        .context("failed to parse integer")
 }
---
+++ src/library.rs
@@ -0,0 +1,145 @@
+use std::mem;
+
+#[derive(Debug, Clone)]
+enum State<T, const N: usize> {
+    Begin,
+    Buffered([T; N]),
+    Done,
+}
+
+impl<T, const N: usize> State<T, N> {
+    fn take(&mut self) -> Self {
+        mem::replace(self, State::Done)
+    }
+}
+
+#[derive(Debug)]
+pub struct Windows<I: Iterator, const N: usize> {
+    iter: I,
+    state: State<I::Item, N>,
+}
+
+impl<I: Iterator, const N: usize> Windows<I, N> {
+    fn compute_size_hint(inner_size: usize) -> usize {
+        inner_size.saturating_sub(N - 1)
+    }
+}
+
+impl<I: Iterator, const N: usize> Iterator for Windows<I, N>
+where
+    I::Item: Clone,
+{
+    type Item = [I::Item; N];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let buffer = match self.state.take() {
+            State::Begin => brownstone::try_build_iter(&mut self.iter)?,
+            State::Buffered(buffer) => buffer,
+            State::Done => return None,
+        };
+
+        if let Some(next) = self.iter.next() {
+            self.state = State::Buffered(brownstone::build_iter(
+                buffer.iter().skip(1).cloned().chain(Some(next)),
+            ))
+        }
+
+        Some(buffer)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (min, max) = self.iter.size_hint();
+        (
+            Self::compute_size_hint(min),
+            max.map(Self::compute_size_hint),
+        )
+    }
+}
+
+impl<I: Iterator + Clone, const N: usize> Clone for Windows<I, N>
+where
+    I::Item: Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            iter: self.iter.clone(),
+            state: self.state.clone(),
+        }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self.iter.clone_from(&source.iter);
+        self.state.clone_from(&source.state);
+    }
+}
+
+pub struct UseOksAdapter<'a, I, E> {
+    iter: I,
+    error: &'a mut Result<(), E>,
+}
+
+impl<I: Iterator<Item = Result<T, E>>, T, E> Iterator for UseOksAdapter<'_, I, E> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match *self.error {
+            Err(_) => None,
+            Ok(()) => self
+                .iter
+                .next()?
+                .map_err(|err| {
+                    *self.error = Err(err);
+                })
+                .ok(),
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (_, max) = self.iter.size_hint();
+        (0, max)
+    }
+}
+
+pub trait IterExt: Iterator + Sized {
+    fn streaming_windows<const N: usize>(self) -> Windows<Self, N>
+    where
+        Self::Item: Clone,
+    {
+        Windows {
+            iter: self,
+            state: State::Begin,
+        }
+    }
+
+    fn use_oks<T, U, E, F>(self, body: F) -> Result<U, E>
+    where
+        Self: Iterator<Item = Result<T, E>>,
+        F: for<'a> FnOnce(UseOksAdapter<'a, Self, E>) -> U,
+    {
+        let mut err = Ok(());
+
+        let value = body(UseOksAdapter {
+            iter: self,
+            error: &mut err,
+        });
+
+        err.map(|()| value)
+    }
+}
+
+impl<I: Iterator> IterExt for I {}
+
+#[cfg(test)]
+mod iter_ext_tests {
+    use super::*;
+
+    #[test]
+    fn test_streaming_windows() {
+        assert!((0..6).streaming_windows().map(|[a, b, c]| [a, b, c]).eq([
+            [0, 1, 2],
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+        ]))
+    }
+}
