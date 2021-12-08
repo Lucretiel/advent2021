45b5d14997ec1f7d4ff250efe2b8c55527d7f5e1
Nathan West <Lucretiel@gmail.com>
2021-12-07 21:53:16 -0500

    Templates for day 6, 7, 8

---
+++ src/day7.rs
@@ -0,0 +1,7 @@
+pub fn day1(input: &str) -> anyhow::Result<i64> {
+    anyhow::bail!("not implemented yet")
+}
+
+pub fn day2(input: &str) -> anyhow::Result<i64> {
+    anyhow::bail!("not implemented yet")
+}
