8717d987673075e918989c104550aae2cefb8f2b
Nathan West <Lucretiel@gmail.com>
2021-12-07 21:55:48 -0500

    Oops

--- src/day8.rs
+++ src/day8.rs
@@ -1,7 +1,7 @@
-pub fn day1(input: &str) -> anyhow::Result<i64> {
+pub fn part1(input: &str) -> anyhow::Result<i64> {
     anyhow::bail!("not implemented yet")
 }
 
-pub fn day2(input: &str) -> anyhow::Result<i64> {
+pub fn part2(input: &str) -> anyhow::Result<i64> {
     anyhow::bail!("not implemented yet")
 }
